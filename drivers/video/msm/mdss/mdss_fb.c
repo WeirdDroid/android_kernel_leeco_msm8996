2df6ea9d2830f880669987494c833ee5e0d2cc7f
Sultan Alsawaf <sultan@kerneltoast.com>
2018-10-23 13:34:45 -0700

    msm: mdss: Add hack for NEOS display power mode handling
    Change-Id: Iadcdc51c18dbce33e7f37235d183478b21dd862c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2090,6 +2090,12 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 		return ret;
 	}
 	mutex_lock(&mfd->mdss_sysfs_lock);
+
+#ifdef CONFIG_MACH_COMMA
+	if (blank_mode != FB_BLANK_UNBLANK)
+		blank_mode = BLANK_FLAG_LP;
+#endif
+
 	if (mfd->op_enable == 0) {
 		if (blank_mode == FB_BLANK_UNBLANK)
 			mfd->suspend.panel_power_state = MDSS_PANEL_POWER_ON;
